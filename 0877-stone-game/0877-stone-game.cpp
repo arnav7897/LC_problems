8a7df620641dc2ffa880613b9c608b6c9ec1ffc0
ARNAV SHARDA <146191610+arnav7897@users.noreply.github.com>
2026-08-02 11:14:30 +0530

    Time: 19 ms (14.03%), Space: 19.6 MB (27.79%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        return true;
+    int help(vector<vector<int>>&dp ,vector<int>& p ,int s, int e ){
+        if(e<s){
+            return 0;
+        }
+        if(dp[s][e] != -1){
+            return dp[s][e];
+        }
+        int t1 = p[s] + min(help(dp , p , s+2 , e) , help(dp,p,s+1,e-1));
+        int t2 = p[e] + min(help(dp , p , s+1 , e-1) , help(dp, p , s, e-2));
+
+        return dp[s][e] = max(t1 , t2);
+    }
+    bool stoneGame(vector<int>& p) {
+        int sum = 0;
+        int n = p.size();
+        for(int i =0 ;i<n;i++){
+            sum += p[i];
+        }
+        vector<vector<int>> dp(n , vector<int>(n,-1));
+        int a = help(dp,p,0,n-1);
+        return a > sum/2;
     }
 };
