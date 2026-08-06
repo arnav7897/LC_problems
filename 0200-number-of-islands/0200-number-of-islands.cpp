59a95163f1e421ea73dae8494e383496f2b49b23
ARNAV SHARDA <146191610+arnav7897@users.noreply.github.com>
2026-08-06 12:04:42 +0530

    Time: 30 ms (33.15%), Space: 18.7 MB (37.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,55 +3,33 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
-        vector<vector<bool>> vis(n, vector<bool>(m, 0));
-
-        queue<pair<int, int>> q;
-        int c = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && grid[i][j] == '1') {
-                    // traversal
-                    c++;
-                    q.push({i, j});
+        vector<vector<bool>> vis(n , vector<bool>(m , 0));
+        queue<pair<int,int>> q;
+        int ans = 0;
+        int dx[] = {1,-1,0,0};
+        int dy[] = {0 , 0 ,1 ,-1};
+        for(int i =0 ;i<n ;i++){
+            for(int j = 0; j<m ;j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    q.push({i,j});
                     vis[i][j] = true;
-
-                    while (!q.empty()) {
-                        auto [a, b] = q.front();
-                        vis[a][b] = true;
+                    while(!q.empty()){
+                        auto [x,y] = q.front();
                         q.pop();
-
-                        // Down
-                        if (a + 1 < n && !vis[a + 1][b] &&
-                            grid[a + 1][b] == '1') {
-                            q.push({a + 1, b});
-                            vis[a + 1][b] = true;
-                        }
-
-                        // Up
-                        if (a - 1 >= 0 && !vis[a - 1][b] &&
-                            grid[a - 1][b] == '1') {
-                            q.push({a - 1, b});
-                            vis[a - 1][b] = true;
-                        }
-
-                        // Right
-                        if (b + 1 < m && !vis[a][b + 1] &&
-                            grid[a][b + 1] == '1') {
-                            q.push({a, b + 1});
-                            vis[a][b + 1] = true;
-                        }
-
-                        // Left
-                        if (b - 1 >= 0 && !vis[a][b - 1] &&
-                            grid[a][b - 1] == '1') {
-                            q.push({a, b - 1});
-                            vis[a][b - 1] = true;
+                        for(int k =0 ;k<4;k++){
+                            int tx = x + dx[k];
+                            int ty = y + dy[k];
+                            if(tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] != '1' || vis[tx][ty]){
+                                continue;
+                            }
+                            q.push({tx,ty});
+                            vis[tx][ty] = true;
                         }
                     }
+                    ans++;
                 }
             }
         }
-        return c;
+        return ans;
     }
 };
