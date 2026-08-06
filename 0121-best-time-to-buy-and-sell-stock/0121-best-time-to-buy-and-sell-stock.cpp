cff06d749b321c02385fdbf8e18da7a41d226e6c
ARNAV SHARDA <146191610+arnav7897@users.noreply.github.com>
2026-08-06 11:10:09 +0530

    Time: 0 ms (100%), Space: 97.3 MB (88.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int>& p , int i , bool s ,  vector<vector<int>> &dp){
-        int n = p.size();
-        if(i >= n){
-            return 0;
-        }
-        int se = s ? 1:0;
-        if(dp[i][se]!=-1){
-            return dp[i][se];
-        }
-        if(s){
-            return dp[i][se] = max(solve(p , i+1 , false , dp)-p[i] , solve(p , i+1 , s , dp));
-        }else{
-            return dp[i][se] = max(p[i] , solve(p , i+1 , s, dp));
-        }
-    }
     int maxProfit(vector<int>& p) {
         int n = p.size();
-        vector<vector<int>> dp(n+1 , vector<int>(2,0));
-        dp[n][0] = 0;
-        dp[n][1] = 0;
-        for(int i = n-1; i>=0 ;i--){
-            for(int j = 1; j>=0;j--){
-                if(j == 1){
-                    dp[i][j] = max(dp[i+1][j] , p[i]);
-                }else{
-                    dp[i][j] = max(dp[i+1][j+1] - p[i], dp[i+1][j]);
-                }
-            }
+        int next_can_buy = 0;
+        int next_must_sell = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr_must_sell = max(next_must_sell, p[i]);
+            int curr_can_buy = max(next_can_buy, next_must_sell - p[i]);
+            next_must_sell = curr_must_sell;
+            next_can_buy = curr_can_buy;
         }
-        return dp[0][0];   
+
+        return next_can_buy;
     }
 };
