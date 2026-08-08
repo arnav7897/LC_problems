681774da033bd5fe15300bd0ff1c8abaf6d755ef
ARNAV SHARDA <146191610+arnav7897@users.noreply.github.com>
2026-08-08 14:26:10 +0530

    Time: 272 ms (34.32%), Space: 296.5 MB (22.13%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,39 @@
 class Solution {
-public: 
-    int solve(vector<int>& nums , int curr , int prev , vector<vector<int>>&dp){
-        int n = nums.size();
-        if(curr >= n){
-            return 0;
-        }
-        if(dp[curr][prev+1] != -1){
-            return dp[curr][prev+1];
-        }
-        int take = INT_MIN , skip = INT_MIN;
-        if(prev == -1 || nums[curr] > nums[prev]){
-            take = 1+solve(nums , curr +1 , curr,dp);
-        }
-        skip = solve(nums , curr+1 , prev ,dp);
-        return dp[curr][prev+1] = max(skip,take);
-    }
+public:
+    // int solve(vector<int>& nums , int curr , int prev ,
+    // vector<vector<int>>&dp){
+    //     int n = nums.size();
+    //     if(curr >= n){
+    //         return 0;
+    //     }
+    //     if(dp[curr][prev+1] != -1){
+    //         return dp[curr][prev+1];
+    //     }
+    //     int take = INT_MIN , skip = INT_MIN;
+    //     if(prev == -1 || nums[curr] > nums[prev]){
+    //         take = 1+solve(nums , curr +1 , curr,dp);
+    //     }
+    //     skip = solve(nums , curr+1 , prev ,dp);
+    //     return dp[curr][prev+1] = max(skip,take);
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
-        return solve(nums , 0 , -1 , dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+
+                int skip = dp[i + 1][j];
+                int take = 0;
+
+                if (j == 0 || nums[j - 1] < nums[i]) {
+                    take = 1 + dp[i + 1][i + 1];
+                }
+
+                dp[i][j] = max(take, skip);
+            }
+        }
+
+        return dp[0][0];
     }
 };
