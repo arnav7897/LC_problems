40b268485e32910b204b8a459c389062ef1cb947
ARNAV SHARDA <146191610+arnav7897@users.noreply.github.com>
2026-08-19 15:41:47 +0530

    Time: 32 ms (23.61%), Space: 18.7 MB (36.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,27 +3,27 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>> vis(n , vector<bool>(m , 0));
+        vector<vector<bool>> vis(n , vector<bool>(m, false));
         queue<pair<int,int>> q;
         int ans = 0;
         int dx[] = {1,-1,0,0};
-        int dy[] = {0 , 0 ,1 ,-1};
-        for(int i =0 ;i<n ;i++){
-            for(int j = 0; j<m ;j++){
+        int dy[] = {0 , 0, 1 , -1};
+        for(int i =0 ;i<n;i++){
+            for(int j =0 ; j<m ;j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
                     q.push({i,j});
                     vis[i][j] = true;
                     while(!q.empty()){
-                        auto [x,y] = q.front();
+                        auto [a,b] = q.front();
                         q.pop();
-                        for(int k =0 ;k<4;k++){
-                            int tx = x + dx[k];
-                            int ty = y + dy[k];
-                            if(tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] != '1' || vis[tx][ty]){
-                                continue;
+                        for(int i =0 ;i<4;i++){
+                            int x = a + dx[i];
+                            int y = b + dy[i];
+                            
+                            if(x >= 0 && y>=0 && x<n && y<m && !vis[x][y] && grid[x][y] == '1'){
+                                q.push({x,y});
+                                vis[x][y] = true;
                             }
-                            q.push({tx,ty});
-                            vis[tx][ty] = true;
                         }
                     }
                     ans++;
