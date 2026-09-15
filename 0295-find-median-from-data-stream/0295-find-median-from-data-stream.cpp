65eb94089a447a6421f9ff14559cd25c82b73e37
Arnav Sharda <146191610+arnav7897@users.noreply.github.com>
2026-09-15 11:43:18 +0530

    Time: 55 ms (89.81%), Space: 148.6 MB (30.22%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,34 +1,50 @@
 class MedianFinder {
 public:
-    priority_queue<int> left; 
-    priority_queue<int, vector<int>, greater<int>> right; 
-
+    priority_queue<int , vector<int> , greater<int>> right; //min
+    priority_queue<int> left; //max
     MedianFinder() {
+        
     }
-
-    void addNum(int num) {
+    
+   void addNum(int num) {
         if (left.empty() || num <= left.top()) {
             left.push(num);
         } else {
             right.push(num);
         }
+
         if (left.size() > right.size() + 1) {
             right.push(left.top());
             left.pop();
-        } else if (right.size() > left.size() + 1) {
+        }
+        else if (right.size() > left.size() + 1) {
             left.push(right.top());
             right.pop();
         }
     }
 
+    
     double findMedian() {
-        int n = left.size();
         int m = right.size();
-
-        if ((n + m) % 2 == 0) {
-            return ((long long)left.top() + (long long)right.top()) / 2.0;
+        int n = left.size();
+        if((m+n)%2 == 0){
+            int l = left.top();
+            int r = right.top();
+            double ans = (l+r)/2.0;
+            return ans;
+        }else{
+            if(m>n){
+                return right.top();
+            }else{
+                return left.top();
+            }
         }
-
-        return (n > m) ? left.top() : right.top();
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
